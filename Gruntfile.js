ff2bc9febd3da5cce9fd3f6ebd721d69896bf6f0
Jason M. Dudley <j.m.duds@gmail.com>
2015-05-18 10:47:01 -0500

    Finished bundling example.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,30 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        clean: {
-            output: ['ToBeCleaned/*']
+        typescript: {
+            options: {
+                module: 'commonjs'
+            },
+            common: {
+                src: ['./www/PlsRemindMe.Web/js/*.ts'],
+                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.common.js'
+            },
+            binders: {
+                src: ['./www/PlsRemindMe.Web/js/binders/*.ts'],
+                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.binders.js'
+            },
+            models: {
+                src: ['./www/PlsRemindMe.Web/js/models/*.ts'],
+                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.models.js'
+            },
+            viewModels: {
+                src: ['./www/PlsRemindMe.Web/js/viewModels/*.ts'],
+                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.viewModels.js'
+            }
         }
     });
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.registerTask("default", ['clean']);
+
+    grunt.loadNpmTasks("grunt-typescript");
+
+    grunt.registerTask("default", ['typescript']);
 }
