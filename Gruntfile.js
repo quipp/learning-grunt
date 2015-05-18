7ccf62fd2ee2b157d16dbc7063954ff1ecb3d713
Jason M. Dudley <j.m.duds@gmail.com>
2015-05-18 18:31:00 -0500

    Completed basics CSS HTML JS
    Code for minifying, compressing, and linting everything.  All easy -
    just configuration settings in Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,67 +3,45 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        typescript: {
-            options: {
-                module: 'commonjs'
-            },
-            common: {
-                src: ['./www/PlsRemindMe.Web/js/*.ts'],
-                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.common.js'
-            },
-            binders: {
-                src: ['./www/PlsRemindMe.Web/js/binders/*.ts'],
-                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.binders.js'
-            },
-            models: {
-                src: ['./www/PlsRemindMe.Web/js/models/*.ts'],
-                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.models.js'
-            },
-            viewModels: {
-                src: ['./www/PlsRemindMe.Web/js/viewModels/*.ts'],
-                dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.viewModels.js'
+        htmlhint: {
+            templates: {
+                options: {
+                    'attr-lower-case': true,
+                    'attr-value-not-empty': true,
+                    'tag-pair': true,
+                    'tag-self-close': true,
+                    'tagname-lowercase': true,
+                    'id-class-value': true,
+                    'id-class-unique': true,
+                    'src-not-empty': true,
+                    'img-alt-required': true
+                },
+                src: ['www/PlsRemindMe.Web/Templates/**/*.html']
             }
         },
-        jshint: {
-            options: {
-                //force: true
-                '-W069': false, // Failure from typescript enum
-                '-W004': false, // Failure from typescript inheritance
-                '-W058': false, // Failure from typescript missing invoking constructor.
-                reporterOutput: './jshint.txt'
-            },
-            files: ['./www/PlsRemindMe.web/js/_output/*.js']
-        },
-        uglify: {
+        less: {
             development: {
+                options: {
+                    cleancss: true,
+                    compress: false,
+                    modifyVars: {
+                        'color-primary-dark-blue': 'NOT_BLUE'
+                    }
+                },
                 files: [{
                     expand: true,
-                    cwd: './www/PlsRemindMe.Web/js/_output/',
-                    src: '**/*.js',
-                    dest: './www.PlsRemindMe.Web/js/_output/'
+                    cwd: 'www/PlsRemindMe.Web/Content/',
+                    dest: 'www/PlsRemindMe.Web/Content/',
+                    src: ['plsremind.me.*.less'],
+                    ext: '.css',
+                    extDot: 'last'
                 }]
-            },
-            options: {
-                mangle: false,
-                compress: {
-                    drop_console: true
-                },
-                //beautify: true
             }
-        },
-        clean: {
-            options: {
-
-            },
-            files: ['./www/PlsRemindMe.Web/js/_output/*'],
-            folders: ['./www/PlsRemindMe.Web/js/_output/foo']
         }
     });
 
-    grunt.loadNpmTasks('grunt-typescript');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['clean', 'typescript', 'uglify']); //'jshint', 
+    grunt.registerTask('default', ['htmlhint', 'less']);
 }
