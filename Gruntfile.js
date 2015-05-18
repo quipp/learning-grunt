d752c09f72e7f894ebf47099fc257f141f56f610
Jason M. Dudley <j.m.duds@gmail.com>
2015-05-18 16:03:17 -0500

    Finished JavaScript section

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,10 +23,47 @@ module.exports = function(grunt) {
                 src: ['./www/PlsRemindMe.Web/js/viewModels/*.ts'],
                 dest: './www/PlsRemindMe.Web/js/_output/plsRemindMe.viewModels.js'
             }
+        },
+        jshint: {
+            options: {
+                //force: true
+                '-W069': false, // Failure from typescript enum
+                '-W004': false, // Failure from typescript inheritance
+                '-W058': false, // Failure from typescript missing invoking constructor.
+                reporterOutput: './jshint.txt'
+            },
+            files: ['./www/PlsRemindMe.web/js/_output/*.js']
+        },
+        uglify: {
+            development: {
+                files: [{
+                    expand: true,
+                    cwd: './www/PlsRemindMe.Web/js/_output/',
+                    src: '**/*.js',
+                    dest: './www.PlsRemindMe.Web/js/_output/'
+                }]
+            },
+            options: {
+                mangle: false,
+                compress: {
+                    drop_console: true
+                },
+                //beautify: true
+            }
+        },
+        clean: {
+            options: {
+
+            },
+            files: ['./www/PlsRemindMe.Web/js/_output/*'],
+            folders: ['./www/PlsRemindMe.Web/js/_output/foo']
         }
     });
 
-    grunt.loadNpmTasks("grunt-typescript");
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("default", ['typescript']);
+    grunt.registerTask('default', ['clean', 'typescript', 'uglify']); //'jshint', 
 }
